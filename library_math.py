206031089c3de2faf55fba0b965fa8464efbe239
Madhurya <33352186+Madhuryaj@users.noreply.github.com>
2024-02-23 20:18:33 +0000

    Create library_math.py

---
+++ library_math.py
@@ -0,0 +1,20 @@
+import math
+
+#Calculating the Square Root: Use Case: You want to find the square root of a number, which is a common operation in various data mining algorithms.
+
+square_root=math.sqrt(9)
+print(square_root)
+
+#Using Constants (π and e):#Use Case: You need π for calculating the area of a circle or e for
+# certain statistical distributions or algorithms.
+
+r=2
+area_of_circle=math.pi * r**2
+print(area_of_circle)
+
+#Logarithmic Functions: Use Case: Logarithms are widely used in
+# data scaling, data normalization, or in certain algorithms.
+
+log_number=10
+log_of=math.log(log_number,10)
+print(log_of)
